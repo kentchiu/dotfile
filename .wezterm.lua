c6024aee48108b460ba8d8ed755509a5c16927bc
Kent Chiu <kent.cwg@gmail.com>
2024-05-18 09:47:28 +0800

    update wezterm

--- .wezterm.lua
+++ .wezterm.lua
@@ -1,45 +1,40 @@
-local wezterm = require 'wezterm'
+local wezterm = require("wezterm")
 
 local config = {}
 
--- In newer versions of wezterm, use the config_builder which will
--- help provide clearer error messages
+-- 使用 config_builder 提供更清晰的錯誤信息
 if wezterm.config_builder then
-  config = wezterm.config_builder()
+	config = wezterm.config_builder()
 end
 
--- This is where you actually apply your config choices
-config.default_domain = 'WSL:Arch'
-
---config.font = wezterm.font 'JetBrains Mono'
--- config.font =  wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
---config.font =  wezterm.font('JetBrains Mono', { weight = 'Bold', italic = true })
+-- 字體設置，使用 JetBrains Mono 和 Symbols Nerd Font Mono 作為備選字體
 config.font = wezterm.font_with_fallback({
-  'JetBrains Mono',
-  { family = 'Symbols Nerd Font Mono', scale =0.75}
+	"JetBrains Mono",
+	{ family = "Symbols Nerd Font Mono", scale = 0.75 },
 })
 
--- Enable the scrollbar.
--- It will occupy the right window padding space.
--- If right padding is set to 0 then it will be increased
--- to a single cell width
+-- 設置字體大小
+config.font_size = 12.0
+
+-- 禁用更新檢查
 config.check_for_updates = false
+-- 啟用滾動條
 config.enable_scroll_bar = true
-config.audible_bell = 'Disabled'
-config.window_close_confirmation = 'NeverPrompt'
+-- 禁用音響提示
+config.audible_bell = "Disabled"
+-- 關閉窗口時不提示確認
+config.window_close_confirmation = "NeverPrompt"
 
---config.font_size = 10.0
+-- 配色方案
+config.color_scheme = "One Dark (Gogh)"
 
--- theme
---config.color_scheme = 'JetBrains Darcula'
--- config.color_scheme = 'One Dark (Gogh)'
--- config.color_scheme = 'One Half Black (Gogh)'
--- config.color_scheme = 'Orangish (terminal.sexy)'
--- config.color_scheme = 'Darcula (base16)'
-
-
--- if wezterm.target_triple == "x86_64-pc-windows-msvc" then
--- else
--- end
+-- 平台特定配置
+if wezterm.target_triple == "x86_64-pc-windows-msvc" then
+	-- Windows 平台特定配置
+	config.default_domain = "WSL:Arch"
+elseif wezterm.target_triple == "x86_64-apple-darwin" then
+	-- macOS 平台特定配置
+	-- 此處可添加 macOS 平台的特定設置
+end
 
 return config
