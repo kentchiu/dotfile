d2e9b81bd93aedda58d7f8c49e78a2a8f81346fd
Kent Chiu <kent.cwg@gmail.com>
2024-12-18 17:48:28 +0800

    update snacks config

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -34,13 +34,6 @@ return {
       end,
       desc = "Delete Buffer",
     },
-    -- {
-    --   "<leader>gg",
-    --   function()
-    --     Snacks.lazygit()
-    --   end,
-    --   desc = "Lazygit",
-    -- },
     {
       "<leader>gb",
       function()
@@ -123,43 +116,4 @@ return {
     --   end,
     -- },
   },
-  init = function()
-    vim.api.nvim_create_autocmd("User", {
-      pattern = "VeryLazy",
-      callback = function()
-        -- test snacks features
-        --[[
-        for i = 1, 10 do
-          vim.defer_fn(function()
-            vim.notify("Hello " .. i, "info", { id = "test" })
-            Snacks.notify.info("Hello " .. i)
-            dd(i)
-          end, i * 500)
-        end
-        --]]
-
-        -- Setup some globals for debugging (lazy-loaded)
-        _G.dd = function(...)
-          Snacks.debug.inspect(...)
-        end
-        _G.bt = function()
-          Snacks.debug.backtrace()
-        end
-        vim.print = _G.dd -- Override print to use snacks for `:=` command
-
-        -- Create some toggle mappings
-        -- Snacks.toggle.option("spell", { name = "Spelling" }):map("<leader>us")
-        -- Snacks.toggle.option("wrap", { name = "Wrap" }):map("<leader>uw")
-        -- Snacks.toggle.option("relativenumber", { name = "Relative Number" }):map("<leader>uL")
-        -- Snacks.toggle.diagnostics():map("<leader>ud")
-        -- Snacks.toggle.line_number():map("<leader>ul")
-        -- Snacks.toggle
-        --   .option("conceallevel", { off = 0, on = vim.o.conceallevel > 0 and vim.o.conceallevel or 2 })
-        --   :map("<leader>uc")
-        -- Snacks.toggle.treesitter():map("<leader>uT")
-        -- Snacks.toggle.option("background", { off = "light", on = "dark", name = "Dark Background" }):map("<leader>ub")
-        -- Snacks.toggle.inlay_hints():map("<leader>uh")
-      end,
-    })
-  end,
 }
