a039c225788ed3f4a3aa9a12b4dcd348c632cff4
Kent Chiu <kent.cwg@gmail.com>
2024-12-18 17:52:53 +0800

    remove comment

--- nvim/lua/plugins/snacks.lua
+++ nvim/lua/plugins/snacks.lua
@@ -97,23 +97,5 @@ return {
       end,
       desc = "Prev Reference",
     },
-    -- {
-    --   "<leader>N",
-    --   desc = "Neovim News",
-    --   function()
-    --     Snacks.win({
-    --       file = vim.api.nvim_get_runtime_file("doc/news.txt", false)[1],
-    --       width = 0.6,
-    --       height = 0.6,
-    --       wo = {
-    --         spell = false,
-    --         wrap = false,
-    --         signcolumn = "yes",
-    --         statuscolumn = " ",
-    --         conceallevel = 3,
-    --       },
-    --     })
-    --   end,
-    -- },
   },
 }
