34e68a43902ad52838bfc2f14aa0e275bc603210
Kent Chiu <kent.cwg@gmail.com>
2025-04-13 11:05:28 +0800

    sync options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -4,8 +4,8 @@
 
 vim.opt.relativenumber = true -- Relative line numbers
 vim.opt.swapfile = false -- disable swapfiles
--- vim.opt.conceallevel = 0 -- disable conceal
 vim.opt.inccommand = "split" -- Incremental live completion
+vim.opt.spelllang = "en_us,cjk" -- cjk for Chinese, Japanese and korean
 
 -- vim.opt.listchars = { space = "·", tab = "⇥ ", eol = "↵" }
 vim.opt.listchars = {
@@ -18,10 +18,19 @@ vim.opt.listchars = {
   nbsp = "␣",
 }
 
--- set to `true` to follow the main branch
--- you need to have a working rust toolchain to build the plugin
--- in this case.
--- vim.g.lazyvim_blink_main = false
--- then you need to set the option below.
-vim.g.lazyvim_picker = "snacks"
+-- vim.opt.diffopt = "filler,internal,closeoff,algorithm:histogram,context:5,linematch:60"
+-- example here: https://vimways.org/2018/the-power-of-diff/
+vim.opt.diffopt = {
+  "internal", -- use internal diff
+  "filler", -- fill diff with empty row which make diff more Intuitive
+  "closeoff", -- auto close diff when buffer close
+  "context:12", -- diff conext line
+  "algorithm:histogram", -- suit for small chunk
+  "linematch:200",
+  "indent-heuristic", -- smart indent
+  -- "iwhite", -- this one, it doesn't fit all cases.
+}
+
+-- SSH remove copy support.
+-- https://github.com/neovim/neovim/pull/33021
 -- vim.g.clipboard = "osc52"
